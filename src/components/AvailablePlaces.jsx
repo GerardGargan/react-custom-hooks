e217b0b8630a483e7d1ed90823c91ac824756635
Gerard Gargan <122169905+GerardGargan@users.noreply.github.com>
2024-10-04 19:36:46 +0100

    handle sorting and fetching available places with custom hook

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,42 +1,29 @@
-import { useState, useEffect } from 'react';
-
 import Places from './Places.jsx';
 import Error from './Error.jsx';
 import { sortPlacesByDistance } from '../loc.js';
 import { fetchAvailablePlaces } from '../http.js';
+import { useFetch } from '../hooks/useFetch.js';
 
-export default function AvailablePlaces({ onSelectPlace }) {
-  const [isFetching, setIsFetching] = useState(false);
-  const [availablePlaces, setAvailablePlaces] = useState([]);
-  const [error, setError] = useState();
+ export default function AvailablePlaces({ onSelectPlace }) {
 
-  useEffect(() => {
-    async function fetchPlaces() {
-      setIsFetching(true);
+async function fetchSortedPlaces() {
+  const places = await fetchAvailablePlaces();
 
-      try {
-        const places = await fetchAvailablePlaces();
+  return new Promise((resolve) => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const sortedPlaces = sortPlacesByDistance(
+        places,
+        position.coords.latitude,
+        position.coords.longitude
+      );
 
-        navigator.geolocation.getCurrentPosition((position) => {
-          const sortedPlaces = sortPlacesByDistance(
-            places,
-            position.coords.latitude,
-            position.coords.longitude
-          );
-          setAvailablePlaces(sortedPlaces);
-          setIsFetching(false);
-        });
-      } catch (error) {
-        setError({
-          message:
-            error.message || 'Could not fetch places, please try again later.',
-        });
-        setIsFetching(false);
-      }
-    }
+      resolve(sortedPlaces);
+    });
+  });
+}
+  
 
-    fetchPlaces();
-  }, []);
+  const { fetchedData: availablePlaces, error, isFetching } = useFetch(fetchSortedPlaces, []);
 
   if (error) {
     return <Error title="An error occurred!" message={error.message} />;
